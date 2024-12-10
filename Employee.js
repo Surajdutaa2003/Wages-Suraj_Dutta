e5fbf9960f7e4644566fd1ecddc526572b25f77c
Surajdutaa2003 <145049082+Surajdutaa2003@users.noreply.github.com>
2024-12-10 11:33:38 +0530

    Update Employee.js

--- Employee.js
+++ Employee.js
@@ -19,23 +19,25 @@ const HOURLY_WAGE = 20;  // Hourly Wage
 const PART_TIME_HOURS = 4;  // Part-time work hours
 const FULL_TIME_HOURS = 8;  // Full-time work hours
 
-let workType = Math.floor(Math.random() * 3);  // Randomly choose: 0, 1, or 2
-
-let dailyWage;
-
-switch (workType) {
-    case PART_TIME:
-        dailyWage = PART_TIME_HOURS * HOURLY_WAGE;
-        console.log(`Part Time: Worked ${PART_TIME_HOURS} hours, Daily Wage: $${dailyWage}`);
-        break;
-    case FULL_TIME:
-        dailyWage = FULL_TIME_HOURS * HOURLY_WAGE;
-        console.log(`Full Time: Worked ${FULL_TIME_HOURS} hours, Daily Wage: $${dailyWage}`);
-        break;
-    case NO_TIME:
-        dailyWage = 0;
-        console.log("No Work: Employee is absent today");
-        break;
-    default:
-        console.log("Invalid input");
+let dailyWage = 0;
+if (employeeStatus === IS_PRESENT) {
+    let workType = Math.floor(Math.random() * 3);  // Randomly choose: 0, 1, or 2
+    switch (workType) {
+        case PART_TIME:
+            dailyWage = PART_TIME_HOURS * HOURLY_WAGE;
+            console.log(`Part Time: Worked ${PART_TIME_HOURS} hours, Daily Wage: $${dailyWage}`);
+            break;
+        case FULL_TIME:
+            dailyWage = FULL_TIME_HOURS * HOURLY_WAGE;
+            console.log(`Full Time: Worked ${FULL_TIME_HOURS} hours, Daily Wage: $${dailyWage}`);
+            break;
+        case NO_TIME:
+            dailyWage = 0;
+            console.log("No Work: Employee is absent today");
+            break;
+        default:
+            console.log("Invalid input");
+    }
 }
+
+
